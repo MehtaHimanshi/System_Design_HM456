8960a2716d87b42fd70ed567c423081d2ddae376
MehtaHimanshi <mehtahimanshi456@gmail.com>
2026-01-21 01:22:25 +0530

    Add EcommerceWebsite project with wearyouridentity folder

---
+++ EcommerceWebsite/Ecommerce/wearyouridentity/src/main/java/com/ecommerce/wearyouridentity/WearyouridentityApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.wearyouridentity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WearyouridentityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WearyouridentityApplication.class, args);
+	}
+
+}
---
+++ EcommerceWebsite/Ecommerce/wearyouridentity/src/test/java/com/ecommerce/wearyouridentity/WearyouridentityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.wearyouridentity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WearyouridentityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
